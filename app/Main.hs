da76b43aae0ad004a468ed35ae9507172376e447
Manuel <msimoni+sky@gmail.com>
2024-09-19 06:52:19 +0000

    Serialize contract

--- app/Main.hs
+++ app/Main.hs
@@ -3,24 +3,23 @@
 
 module Main where
 
-import AuctionValidator
+import ClientValidator
 import Data.ByteString qualified as B
+import PlutusTx.Builtins.HasOpaque (stringToBuiltinByteString)
 import Data.ByteString.Base16 qualified as Base16
 import Data.ByteString.Short qualified as B
 import PlutusLedgerApi.V2 qualified as V2
 
-
 main :: IO ()
 main = B.writeFile "validator.uplc" . Base16.encode $ B.fromShort serialisedScript
   where
-    script = auctionValidatorScript params
+    script = clientValidatorScript params
     serialisedScript = V2.serialiseCompiledCode script
     params =
-        AuctionParams
-            { apSeller = V2.PubKeyHash "addr_test1vqe09nt0rxgwn83upxuhqzs4aqrzdjqmhrh5l4g5hh4kc6qsncmku"
-            , -- The asset to be auctioned is 10000 lovelaces
-              apAsset = V2.singleton V2.adaSymbol V2.adaToken 10000
-            , -- The minimum bid is 100 lovelaces
-              apMinBid = 100
-            , apEndTime = 4102416000000 -- 01/01/2100
+        ClientParams
+            { offerer = V2.PubKeyHash "addr_test1vqe09nt0rxgwn83upxuhqzs4aqrzdjqmhrh5l4g5hh4kc6qsncmku"
+            , publisher = V2.PubKeyHash "addr_test1vqe09nt0rxgwn83upxuhqzs4aqrzdjqmhrh5l4g5hh4kc6qsncmku"
+            , operators = MultiSigPubKey [] 0
+            , asset = V2.singleton V2.adaSymbol V2.adaToken 10000
+            , challenge = Challenge (stringToBuiltinByteString "foo")
             }
