22b02ee44d5c0b176c92b33d94dfc3310a6306bf
Manuel <msimoni+sky@gmail.com>
2024-09-19 08:29:31 +0000

    Add 3 pubkeys

--- app/Main.hs
+++ app/Main.hs
@@ -11,8 +11,11 @@ import Data.ByteString.Base16 qualified as Base16
 import Data.ByteString.Short qualified as B
 import PlutusLedgerApi.V2 qualified as V2
 import Text.Hex (Text, ByteString, decodeHex)
+import Data.Maybe (fromJust)
 
 {-
+Created with https://cyphr.me/ed25519_tool/ed.html
+
 challenge: "hello world" (UTF-8)
 
 priv, pub, sig:
@@ -24,6 +27,10 @@ B48CD89C3CCA7DD334647C3F54816E52DA0002618DA55803CD524D7ABF272EE8CE771D30243772FB
 03A4BEF77A4904CF25EC8F815FCC6D5EF91609644187926D1F28DCDED7DE89C0
 DE3B4832CC5DD4B8412926C23BDCDFD4503E808A37B069A7C42DE8749AF23D81
 6D00BEE751616589F9E243B83E49218F23158E8878A6FD62500A3E10A967A2A4ED41B24E7DC96E088CAC5AB1EEEAA5EFE5A36B864EE6FF0FCFD5E2D710E3F509
+
+906A12BD4D0DE996BDA37589FAD3B8A0755A3B3EDA3EB6E86E1437839A193A36
+70F57BF6004DAF872A256E1482F9A0FDE9E45C523A1CD4A20E9CA3F78BB36565
+E9337D0623A15F252B65532BCB68CEECF821068E83559E0201295B430D4C3C3750AD90185C677D56C2F88EB207EC83D44DDEDBA3B4D9B07F12A33B9B22136A0E
 -}
 
 hexStringToBuiltinByteString :: Text -> Maybe BuiltinByteString
@@ -34,11 +41,14 @@ main = B.writeFile "validator.uplc" . Base16.encode $ B.fromShort serialisedScri
   where
     script = clientValidatorScript params
     serialisedScript = V2.serialiseCompiledCode script
+    pub1 = PubKey (fromJust (hexStringToBuiltinByteString "43004B8F43FAF0E3EAAAF55BB41DC53CBF09E42884267BAF8C1EA6903819122C"))
+    pub2 = PubKey (fromJust (hexStringToBuiltinByteString "DE3B4832CC5DD4B8412926C23BDCDFD4503E808A37B069A7C42DE8749AF23D81"))
+    pub3 = PubKey (fromJust (hexStringToBuiltinByteString "70F57BF6004DAF872A256E1482F9A0FDE9E45C523A1CD4A20E9CA3F78BB36565"))
     params =
         ClientParams
             { offerer = V2.PubKeyHash "addr_test1vqe09nt0rxgwn83upxuhqzs4aqrzdjqmhrh5l4g5hh4kc6qsncmku"
             , publisher = V2.PubKeyHash "addr_test1vqe09nt0rxgwn83upxuhqzs4aqrzdjqmhrh5l4g5hh4kc6qsncmku"
-            , operators = MultiSigPubKey [] 0
+            , operators = MultiSigPubKey [pub1, pub2, pub3] 2
             , asset = V2.singleton V2.adaSymbol V2.adaToken 10000
-            , challenge = Challenge (stringToBuiltinByteString "foo")
+            , challenge = Challenge (stringToBuiltinByteString "hello world")
             }
